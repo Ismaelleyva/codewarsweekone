ff7757fc677bee2e798e4dbcb37ac67856ea7dc1
Ismael Leyva <2020ileyva@01889.dwight.edu>
2019-02-20 13:10:58 -0500

    week2

---
+++ week2.py
@@ -0,0 +1,12 @@
+def unique_in_order(iterable):
+    arr = list(iterable)
+    x = []
+    def while_print(arr):
+        count == 0
+        while len(arr) > count:
+            if iterable >
+            count = count + 1
+        print(x)
+
+
+    pass
